0ed6a784e9ce156a4f9453ee73c4d379762f49d5
JN513 <jn5.guape@gmail.com>
2022-11-28 12:38:16 -0300

    Arrumando CORS

--- routes/router.go
+++ routes/router.go
@@ -3,20 +3,27 @@ package routes
 import (
 	controller "data_logger/controllers"
 	"fmt"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 )
 
 func InitServer() {
-	http.HandleFunc("/", controller.GetAllData) // Update this line of code
-	http.HandleFunc("/filter", controller.GetData)
-	http.HandleFunc("/delete", controller.DeleteData)
-	http.HandleFunc("/count", controller.CountData)
-	http.HandleFunc("/find/count", controller.FilterAndCountData)
-	http.HandleFunc("/itens", controller.ListItens)
+	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", controller.GetAllData) // Update this line of code
+	mux.HandleFunc("/filter", controller.GetData)
+	mux.HandleFunc("/delete", controller.DeleteData)
+	mux.HandleFunc("/count", controller.CountData)
+	mux.HandleFunc("/find/count", controller.FilterAndCountData)
+	mux.HandleFunc("/itens", controller.ListItens)
+
+	// cors.Default() setup the middleware with default options being
+	// all origins accepted with simple methods (GET, POST). See
+	// documentation below for more options.
+	handler := cors.Default().Handler(mux)
 	fmt.Printf("Starting server at port 8883\n")
-	if err := http.ListenAndServe("0.0.0.0:8883", nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8883", handler); err != nil {
 		log.Fatal(err)
 	}
 }
